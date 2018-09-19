4ec4c56243727fda32ffb384d710d391bc58c81d
Patrick Boyd <Patrick_Boyd@Dell.com>
2018-09-19 17:54:31 +0000

    Switch to Jest for unit tests

--- test/main.js
+++
@@ -1,8 +0,0 @@
-var package = require('../package');
-var spf = require('../index');
-
-module.exports.constants = function(assert) {
-    assert.ok(typeof spf.version == 'string');
-    assert.ok(typeof spf.SPFValidator == 'function');
-    assert.done();
-}
