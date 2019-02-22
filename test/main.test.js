95618d2b3821288f1ff95b2e5c2d50e2a2e1de9a
Patrick Boyd <pboyd04@gmail.com>
2019-02-22 17:15:14 -0600

    Remove jest test cases

--- test/main.test.js
+++
@@ -1,8 +0,0 @@
-var spf = require('../index');
-
-test('Version is string', () => {
-  expect(typeof spf.version).toBe('string');
-});
-test('SPFValidator is function', () => {
-  expect(typeof spf.SPFValidator).toBe('function');
-});
